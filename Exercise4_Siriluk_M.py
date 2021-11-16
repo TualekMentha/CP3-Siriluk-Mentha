0fe9e3769fc9a25b75b3bdef24fe42c75f495969
TualekMentha <siriluktualek55@gmail.com>
2021-11-16 20:33:05 +0100

    Exercise4

---
+++ Exercise4_Siriluk_M.py
@@ -0,0 +1,9 @@
+F_eng = 88.5
+G_bus = 70.5
+ICS = 75.5
+CP = 90.5
+print("--- Your Score ---")
+print("Foundation English :",(F_eng))
+print("General Business :", (G_bus))
+print("Introduction to Computer Systems :", (ICS))
+print("Computer Programming :", (CP))
