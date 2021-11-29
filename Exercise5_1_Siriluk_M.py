5f8d94276a3b26cd9c4d78ae6a332103db28a98d
TualekMentha <siriluktualek55@gmail.com>
2021-11-29 21:00:17 +0100

    Create Exercise5
    Create Exercise5

---
+++ Exercise5_1_Siriluk_M.py
@@ -0,0 +1,6 @@
+N1 = int(input("Enter number 1st"))
+N2 = int(input("Enter number 2nd"))
+print(N1,"+",N2,"=",N1+N2)
+print(N1,"-",N2,"=",N1-N2)
+print(N1,"*",N2,"=",N1*N2)
+print(N1,"/",N2,"=",N1/N2)
