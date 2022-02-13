34234886591c517bb5686c686d2b4a6d4d41a53f
Siriluk Mentha <94185696+TualekMentha@users.noreply.github.com>
2022-02-13 12:14:06 +0100

    Delete WhileExample2.py

--- WhileExample2.py
+++
@@ -1,6 +0,0 @@
-usernameInput = input("Username : ")
-passwordInput = input("Password : ")
-while usernameInput != "admin" or passwordInput != "1234":
-    usernameInput = input("Username : ")
-    passwordInput = input("Password : ")
-print("Done!")
